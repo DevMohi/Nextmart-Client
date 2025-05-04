232dae5b2aff01423d6c857844dc54015380c58b
DevMohi <morphex72@gmail.com>
2025-05-05 02:11:14 +0600

    functional table pagination added

--- src/components/modules/shop/product/index.tsx
+++ src/components/modules/shop/product/index.tsx
@@ -10,6 +10,7 @@ import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { useState } from "react";
 import DiscountModal from "./DiscountModal";
+import TablePagination from "@/components/ui/core/NMTable/TablePagination";
 
 const ManageProducts = ({ products }: { products: IProduct[] }) => {
   const router = useRouter();
@@ -157,6 +158,7 @@ const ManageProducts = ({ products }: { products: IProduct[] }) => {
         </div>
       </div>
       <NMTable columns={columns} data={products || []} />
+      <TablePagination />
     </div>
   );
 };
